4f59839c52a3ab1fb878fbef31458979ad340a56
Administrator <Administrator@LAPTOP-GP5VI7AI>
2018-03-16 21:14:11 +0000

    Editing Objects Class

--- GladeRunner V2.0/Objects.h
+++ GladeRunner V2.0/Objects.h
@@ -1,11 +1,19 @@
 #ifndef OBJECTS_H
 #define OBJECTS_H
 
+#include "GameSystem.h"
+
 class Objects
 {
+private:
+	SDL_Rect dest;
+	SDL_Rect src;
+	SDL_Texture* tex;
 public:
 	Objects();
 	~Objects();
+	SDL_Rect GetDest() const { return dest; }
+	SDL_Rect GetSource() const { return src; }
 };
 
 #endif //OBJECTS_H
